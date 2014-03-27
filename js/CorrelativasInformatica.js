9b0e80765c0f8ce094dfd4df2c2fc29c42daf16a
Juan Gesino <juangesino@gmail.com>
2014-03-27 19:28:07 -0300

    source files

---
+++ js/CorrelativasInformatica.js
@@ -0,0 +1,186 @@
+
+var array1=["1","Analisis Matematico I"];
+array.push(array1);
+
+var array2=["2","Algebra I"];
+array.push(array2);
+
+var array3=["3","Intro. a la Programacion I"];
+array.push(array3);
+
+var array4=["4","Intro. a la Ingenieria"];
+array.push(array4);
+
+var array5=["5","Intro. a la Filosofia"];
+array.push(array5);
+
+var array6=["6","Quimica I"];
+array.push(array6);
+
+var array7=["7","Analisis Matematico II","1"];
+array.push(array7);
+
+var array8=["8","Algebra II","2"];
+array.push(array8);
+
+var array9=["9","Intro. a la Programacion II","3"];
+array.push(array9);
+
+var array10=["10","Fisica I","1"];
+array.push(array10);
+
+var array11=["11","Logica"];
+array.push(array11);
+
+var array12=["12","Analisis Matematico III","7"];
+array.push(array12);
+
+var array13=["13","Algebra III","8"];
+array.push(array13);
+
+var array14=["14","Fisica II","10"];
+array.push(array14);
+
+var array15=["15","Estadistica I","1"];
+array.push(array15);
+
+var array16=["16","Algoritmos y estructuras de datos","9"];
+array.push(array16);
+
+var array17=["17","Antropologia","5"];
+array.push(array17);
+
+var array18=["18","Analisis Matematico IV","12"];
+array.push(array18);
+
+var array19=["19","Matematica Discreta","13"];
+array.push(array19);
+
+var array20=["20","Estadistica II","15"];
+array.push(array20);
+
+var array21=["21","Tecnicas de Comunicacion"];
+array.push(array21);
+
+var array22=["22","Fisica III","10","12"];
+array.push(array22);
+
+var array23=["23","Analisis y dise&ntilde;o de algoritmos","16"];
+array.push(array23);
+
+var array24=["24","Medios de representacion"];
+array.push(array24);
+
+var array25=["25","Bases de datos y recursos de la informacion","16"];
+array.push(array25);
+
+var array26=["26","Investigacion Operativa","8","20"];
+array.push(array26);
+
+var array27=["27","Teoria de la empresa","4","15"];
+array.push(array27);
+
+var array28=["28","Laboratorio I","16"];
+array.push(array28);
+
+var array29=["29","Etica general","17"];
+array.push(array29);
+
+var array30=["30","Logica y circuitos digitales","22"];
+array.push(array30);
+
+var array31=["31","Dise&ntilde;o de sistemas","23","25"];
+array.push(array31);
+
+var array32=["32","Lenguajes de programacion","23"];
+array.push(array32);
+
+var array33=["33","Organizacion de computadoras","30"];
+array.push(array33);
+
+var array34=["34","Redes de comunicacion de datos","30"];
+array.push(array34);
+
+var array35=["35","Laboratorio II","28"];
+array.push(array35);
+
+var array36=["36","Teologia I","29"];
+array.push(array36);
+
+var array37=["37","Introduccion a la computacion","16","30"];
+array.push(array37);
+
+var array38=["38","Ingenieria de sistemas","31"];
+array.push(array38);
+
+var array39=["39","Sistemas Operativos","32","33"];
+array.push(array39);
+
+var array40=["40","Aseguramiento de la calidad del software","38"];
+array.push(array40);
+
+var array41=["41","Contabilidad y presupuestos","27"];
+array.push(array41);
+
+var array42=["42","Laboratorio III (anual)","31"];
+array.push(array42);
+
+var array43=["43","Teologia II","36"];
+array.push(array43);
+
+var array44=["44","Optativa I"];
+array.push(array44);
+
+var array45=["45","Finanzas y evaluacion de proyectos","41"];
+array.push(array45);
+
+var array46=["46","Optativa II"];
+array.push(array46);
+
+var array47=["47","Optativa III"];
+array.push(array47);
+
+var array48=["48","Practiva Profesional","6to Cuatrimestre cursado"];
+array.push(array48);
+
+var array49=["49","Doctrina Social de la Iglesia","43"];
+array.push(array49);
+
+var array50=["50","Laboratorio III (anual)","31"];
+array.push(array50);
+
+var array51=["51","Legal","27"];
+array.push(array51);
+
+var array52=["52","Optativa IV"];
+array.push(array52);
+
+var array53=["53","Trabajo de grado (anual)","7mo cuatrimestre cursado"];
+array.push(array53);
+
+var array54=["54","Direccion de Proyectos (anual)","38"];
+array.push(array54);
+
+var array55=["55","Optativa V"];
+array.push(array55);
+
+var array56=["56","Etica Profesional","49"];
+array.push(array56);
+
+var array57=["57","Economia Politica","45"];
+array.push(array57);
+
+var array58=["58","Sistemas de informacion gerencial","38"];
+array.push(array58);
+
+var array59=["59","Direccion de proyectos (anual)","38"];
+array.push(array59);
+
+var array60=["60","Optativa VI"];
+array.push(array60);
+
+var array61=["61","Trabajo de grado (anual)","7mo cuatrimestre cursado"];
+array.push(array61);
+
+var array62=["62","Factor Humano","27"];
+array.push(array62);
---
+++ js/correlativas.js
@@ -0,0 +1,11 @@
+
+var array = [];
+
+var array1 = ['1', 'analisis2', 'analisis1'];
+array.push(array1);
+
+var array2 = ['2', 'analisis3', 'analisis2'];
+array.push(array2);
+
+var array3 = ['3', 'analisis1'];
+array.push(array3);
