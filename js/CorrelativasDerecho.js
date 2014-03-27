3c33a6301508bef8e1d00576ed5a29e1314ba1da
Juan Gesino <juangesino@gmail.com>
2014-03-27 19:43:41 -0300

    source files

---
+++ js/CorrelativasDerecho.js
@@ -0,0 +1,207 @@
+var array1=["1","Introduccion al Derecho"];
+array.push(array1);
+
+var array2=["2","Exprecion Juridica y Argumentacion"];
+array.push(array2);
+
+
+var array3=["3","Ciencia Politica"];
+array.push(array3);
+
+
+var array4=["4","Seminario Operativo 1"];
+array.push(array4);
+
+
+var array5=["5","Derecho Romano e Instituciones Juridicas Contemporaneas"];
+array.push(array5);
+
+
+var array6=["6","Introduccion a la Filosofia y Antropologia"];
+array.push(array6);
+
+
+var array7=["7","Seminario Optativo 1"];
+array.push(array7);
+
+
+var array8=["8","Instituciones de Derecho Civil y Comercial"];
+array.push(array8);
+
+
+var array9=["9","Derecho Penal 1"];
+array.push(array9);
+
+
+var array10=["10","Teologia Dogmatica"];
+array.push(array10);
+
+
+var array11=["11","Taller de Discusion de Casos en Equipos"];
+array.push(array11);
+
+
+var array12=["12","Derecho Constitucional 1"];
+array.push(array12);
+
+
+var array13=["13","Derecho Penal 2","Derecho Penal 1"];
+array.push(array13);
+
+
+var array14=["14","Derecho de las Obligaciones"];
+array.push(array14);
+
+
+var array15=["15","Etica y Metafisica"];
+array.push(array15);
+
+
+var array16=["16","Seminario de Arte y Literatura"];
+array.push(array16);
+
+
+var array17=["17","Derecho Constitucional 2"];
+array.push(array17);
+
+
+var array18=["18","Derecho Penal 3","Derecho Penal 1"];
+array.push(array18);
+
+
+var array19=["19","Derecho Internacional Publico"];
+array.push(array19);
+
+
+var array20=["20","Derecho de Da&ntilde;os"];
+array.push(array20);
+
+
+var array21=["21","Economia Politica y de la Empresa"];
+array.push(array21);
+
+var array22=["22","Seminario Optativo 2"];
+array.push(array22);
+
+
+var array23=["23","Teologia Moral","Teologia Dogmatica"];
+array.push(array23);
+
+
+var array25=["25","Derecho de los Cotratos 1","Derecho de las Obligaciones"];
+array.push(array25);
+
+
+var array26=["26","Derecho Procesal Penal"];
+array.push(array26);
+
+
+var array27=["27","Derecho del Trabajo"];
+array.push(array27);
+
+
+var array28=["28","Doctrina Social de la Iglesia"];
+array.push(array28);
+
+
+var array29=["29","Seminario de Responsabilidad Ciudadana"];
+array.push(array29);
+
+
+var array30=["30","Derecho de los Cotratos 2","Derecho de los Cotratos 1"];
+array.push(array30);
+
+
+var array31=["31","Derecho Societario"];
+array.push(array31);
+
+
+var array32=["32","Derecho Administrativo 1","Derecho Administrativo 2 C"];
+array.push(array32);
+
+
+var array33=["33","Seminario Optativo 3"];
+array.push(array33);
+
+
+var array34=["34","Derecho Procesal Civil"];
+array.push(array34);
+
+
+var array35=["35","Derechos Reales"];
+array.push(array35);
+
+
+var array36=["36","Derecho de los Titulos de Credito"];
+array.push(array36);
+
+
+var array37=["37","Derecho de Familia"];
+array.push(array37);
+
+
+var array38=["38","Derecho Administrativo 2"];
+array.push(array38);
+
+
+var array39=["39","Derecho Concursal"];
+array.push(array39);
+
+
+var array40=["40","Derecho de las Sucesiones"];
+array.push(array40);
+
+
+var array41=["41","Derecho de la Integracion"];
+array.push(array41);
+
+
+var array42=["42","Common Law Institutions 3"];
+array.push(array42);
+
+
+var array43=["43","Seminario de Medios Alternativos de Resolucion de Conflictos"];
+array.push(array43);
+
+
+var array44=["44","Seminario de Analisis Economico del Derecho"];
+array.push(array44);
+
+
+var array45=["45","Clinica Juridica"];
+array.push(array45);
+
+
+var array46=["46","Derecho Internacional Privado"];
+array.push(array46);
+
+
+var array47=["47","Derecho Tributario"];
+array.push(array47);
+
+
+var array48=["48","Estrategia y Prueba en el Asesoramiento y el Litigio"];
+array.push(array48);
+
+
+var array49=["49","Filosofia del Derecho"];
+array.push(array49);
+
+
+var array50=["50","Etica Profesional"];
+array.push(array50);
+
+
+var array51=["51","Modulo Operativo 4"];
+array.push(array51);
+
+
+var array52=["52","Pasantia Profesional Obligatoria 5"];
+array.push(array52);
+
+
+var array53=["53","Programa de Intercambio Internacional"];
+array.push(array53);
+
+
+
