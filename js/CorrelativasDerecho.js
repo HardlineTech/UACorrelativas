ff538f7095755f6b901c22d5854d9783ebd33ef0
Agustin Quintas <agusquintas@hotmail.com>
2015-02-09 19:47:27 -0300

    correlativas y planes de estudios de las 3 nuevs orientaciones de ingenieria industrial

--- js/CorrelativasDerecho.js
+++ js/CorrelativasDerecho.js
@@ -1,209 +1,25 @@
 var array = [];
 
-var array1=["1","Introduccion al Derecho"];
+var array1=["1","Derecho Penal 2","Derecho Penal 1"];
 array.push(array1);
 
-var array2=["2","Exprecion Juridica y Argumentacion"];
+var array2=["2","Derecho Penal 3","Derecho Penal 1"];
 array.push(array2);
 
-
-var array3=["3","Ciencia Politica"];
+var array3=["3","Teologia Moral","Teologia Dogmatica"];
 array.push(array3);
 
-
-var array4=["4","Seminario Operativo 1"];
+var array4=["4","Derecho de los Contratos 1","Derecho de las Obligaciones"];
 array.push(array4);
 
-
-var array5=["5","Derecho Romano e Instituciones Juridicas Contemporaneas"];
+var array5=["5","Derecho de los Contratos 2","Derecho de los Cotratos 1"];
 array.push(array5);
 
-
-var array6=["6","Introduccion a la Filosofia y Antropologia"];
+var array6=["6","Derecho Administrativo 1","Derecho Administrativo 2 C"];
 array.push(array6);
 
-
-var array7=["7","Seminario Optativo 1"];
+var array7=["7","Derecho de las Obligaciones","Derecho Civil 1"];
 array.push(array7);
 
-
-var array8=["8","Instituciones de Derecho Civil y Comercial"];
+var array8=["8","Derecho de Daños","Derecho Civil 1"];
 array.push(array8);
-
-
-var array9=["9","Derecho Penal 1"];
-array.push(array9);
-
-
-var array10=["10","Teologia Dogmatica"];
-array.push(array10);
-
-
-var array11=["11","Taller de Discusion de Casos en Equipos"];
-array.push(array11);
-
-
-var array12=["12","Derecho Constitucional 1"];
-array.push(array12);
-
-
-var array13=["13","Derecho Penal 2","Derecho Penal 1"];
-array.push(array13);
-
-
-var array14=["14","Derecho de las Obligaciones"];
-array.push(array14);
-
-
-var array15=["15","Etica y Metafisica"];
-array.push(array15);
-
-
-var array16=["16","Seminario de Arte y Literatura"];
-array.push(array16);
-
-
-var array17=["17","Derecho Constitucional 2"];
-array.push(array17);
-
-
-var array18=["18","Derecho Penal 3","Derecho Penal 1"];
-array.push(array18);
-
-
-var array19=["19","Derecho Internacional Publico"];
-array.push(array19);
-
-
-var array20=["20","Derecho de Da&ntilde;os"];
-array.push(array20);
-
-
-var array21=["21","Economia Politica y de la Empresa"];
-array.push(array21);
-
-var array22=["22","Seminario Optativo 2"];
-array.push(array22);
-
-
-var array23=["23","Teologia Moral","Teologia Dogmatica"];
-array.push(array23);
-
-
-var array24=["24","Derecho de los Cotratos 1","Derecho de las Obligaciones"];
-array.push(array24);
-
-
-var array25=["25","Derecho Procesal Penal"];
-array.push(array25);
-
-
-var array26=["26","Derecho del Trabajo"];
-array.push(array26);
-
-
-var array27=["27","Doctrina Social de la Iglesia"];
-array.push(array27);
-
-
-var array28=["28","Seminario de Responsabilidad Ciudadana"];
-array.push(array28);
-
-
-var array29=["29","Derecho de los Cotratos 2","Derecho de los Cotratos 1"];
-array.push(array29);
-
-
-var array30=["30","Derecho Societario"];
-array.push(array30);
-
-
-var array31=["31","Derecho Administrativo 1","Derecho Administrativo 2 C"];
-array.push(array31);
-
-
-var array32=["32","Seminario Optativo 3"];
-array.push(array32);
-
-
-var array33=["33","Derecho Procesal Civil"];
-array.push(array33);
-
-
-var array34=["34","Derechos Reales"];
-array.push(array34);
-
-
-var array35=["35","Derecho de los Titulos de Credito"];
-array.push(array35);
-
-
-var array36=["36","Derecho de Familia"];
-array.push(array36);
-
-
-var array37=["37","Derecho Administrativo 2"];
-array.push(array37);
-
-
-var array38=["38","Derecho Concursal"];
-array.push(array38);
-
-
-var array39=["39","Derecho de las Sucesiones"];
-array.push(array39);
-
-
-var array40=["40","Derecho de la Integracion"];
-array.push(array40);
-
-
-var array41=["41","Common Law Institutions 3"];
-array.push(array41);
-
-
-var array42=["42","Seminario de Medios Alternativos de Resolucion de Conflictos"];
-array.push(array42);
-
-
-var array43=["43","Seminario de Analisis Economico del Derecho"];
-array.push(array43);
-
-
-var array44=["44","Clinica Juridica"];
-array.push(array44);
-
-
-var array45=["45","Derecho Internacional Privado"];
-array.push(array45);
-
-
-var array46=["46","Derecho Tributario"];
-array.push(array46);
-
-
-var array47=["47","Estrategia y Prueba en el Asesoramiento y el Litigio"];
-array.push(array47);
-
-
-var array48=["48","Filosofia del Derecho"];
-array.push(array48);
-
-
-var array49=["49","Etica Profesional"];
-array.push(array49);
-
-
-var array50=["50","Modulo Operativo 4"];
-array.push(array50);
-
-
-var array51=["51","Pasantia Profesional Obligatoria 5"];
-array.push(array51);
-
-
-var array52=["52","Programa de Intercambio Internacional"];
-array.push(array52);
-
-
-
---
+++ js/CorrelativasIndustrialMecatronica.js
@@ -0,0 +1,187 @@
+var array = [];
+
+var array1=["1","Analisis Matematico 1"];
+array.push(array1);
+
+var array2=["2","Algebra 1"];
+array.push(array2);
+
+var array3=["3","Programacion 1"];
+array.push(array3);
+
+var array4=["4","Introduccion a la Ingenieria"];
+array.push(array4);
+
+var array5=["5","Filosofia"];
+array.push(array5);
+
+var array6=["6","Quimica 1"];
+array.push(array6);
+
+var array7 =["7", "Analisis Matematico 2", "Analisis Matematico 1"];
+array.push(array7);
+
+var array8=["8","Algebra 2","Algebra 1"];
+array.push(array8);
+
+var array9=["9","Fisica 1","Analisis Matematico 1"];
+array.push(array9);
+
+var array10=["10","Qumica 2","Quimica 1"];
+array.push(array10);
+
+var array11=["11","Calculo Numerico Basico","Analisis Matematico 1", "Algebra 1"];
+array.push(array11);
+
+var array12=["12","Analisis Matematico 3","Analisis Matematico 2"];
+array.push(array12);
+
+var array13=["13","Fisica 2","Fisica 1"];
+array.push(array13);
+
+var array14=["14","Estatica","Fisica 1","Analisis Matematico 1"];
+array.push(array14);
+
+var array15=["15","Estadistica 1","Analisis Matematico 1"];
+array.push(array15);
+
+var array16=["16","Antropologia","Filosofia"];
+array.push(array16);
+
+var array17=["17","Analisis Matematico 4","Analisis Matematico 3","Calculo Numerico Basico"];
+array.push(array17);
+
+var array18=["18","Termodinamica","Fisica 2","Analisis Matematico 2"];
+array.push(array18);
+
+var array19=["19","Estadistica 2","Estadistica 1"];
+array.push(array19);
+
+var array20=["20","Ingenieria Grafica y Dise&ntilde;o"];
+array.push(array20);
+
+var array21=["21","Fisica 3","Fisica 1", "Analisis Matematico 2"];
+array.push(array21);
+
+var array22=["22","Mecanismos","Ingenieria Grafica y Dise&ntilde;o","Mecanica Racional", "Resistencia de Materiales"];
+array.push(array22);
+
+var array23=["23","Mecanica de Fluidos","Analisis Matematico 3","Termodinamica"];
+array.push(array23);
+
+var array24=["24","Teoria de la Empresa","Introduccion a la Ingenieria","Estadistica 1"];
+array.push(array24);
+
+var array25=["25","Investigacion Operativa","Algebra 2","Estadistica 2"];
+array.push(array25);
+
+var array26=["26","Etica General","Antropologia"];
+array.push(array26);
+
+var array27=["27","Tecnicas de la Comunicacion"];
+array.push(array27);
+
+var array28=["28","Tecnologia de Materiales","Quimica 2"];
+array.push(array28);
+
+var array29=["29","Quimica Industrial","Quimica 2"];
+array.push(array29);
+
+var array30=["30","Electrotecnia","Fisica 3"];
+array.push(array30);
+
+var array31=["31","Electromagnetismo","Analisis Matematico 3","Fisica 3"];
+array.push(array31);
+
+var array32=["32","Teologia 1","Introduccion a la Filosofia"];
+array.push(array32);
+
+var array33=["33","Maquinas Electricas","Electrotecnia"];
+array.push(array33);
+
+var array34=["34","Circuitos Electronicos","Analisis Matematico 4","Electrotecnia"];
+array.push(array34);
+
+var array35=["35","Maquinas Termicas 1","Mecanica de Fluidos"];
+array.push(array35);
+
+var array36=["36","Tecnicas de Comunicacion"];
+array.push(array36);
+
+var array37=["37","Teologia 2","Teologia 1"];
+array.push(array37);
+
+var array38=["38","Contabilidad y Presupuestos","Teoria de la Empresa"];
+array.push(array38);
+
+var array39=["39","Maquinas Herramientas","Mecanismos","Tecnologia de Materiales"];
+array.push(array39);
+
+var array40=["40","Instrumentacion y Control","Circuitos Electronicos"];
+array.push(array40);
+
+var array41=["41","Sistemas de Adquisicion y Administracion de Datos","Electromagnetismo","Circuitos Electronicos"];
+array.push(array41);
+
+var array42=["42","Ingenieria de Calidad","Teoria de la Empresa"];
+array.push(array42);
+
+var array43=["43","Mecanica Aplicada","Analisis Matematico 4","Mecanismos"];
+array.push(array43);
+
+var array44=["44","Responsabilidad Social","Etica General"];
+array.push(array44);
+
+var array45=["45","Practica Profesional Supervisada","6to Cuatrimestre Aprobado","7mo Cuatrimestre Cursado"];
+array.push(array45);
+
+var array46=["46","Instalaciones Industriales","Quimica Industrial","Maquinas Electricas","Maquinas Termicas 1"];
+array.push(array46);
+
+var array47=["47","Logistica Empresaria","Produccion 4"];
+array.push(array47);
+
+var array48=["48","Mantenimiento y Seguridad Industrial","Quimica Industrial","Maquinas Herramientas","Ingenieria de Calidad"];
+array.push(array48);
+
+var array49=["49","Proyecto Mecatronica","Mecatronica","Sistemas de Adquisicion y Administracion de Datos", "Actuadores"];
+array.push(array49);
+
+var array50=["50","Legal","Teoria de la Empresa"];
+array.push(array50);
+
+var array51=["51","Etica Profesional","Etica General"];
+array.push(array51);
+
+var array52=["52","Sistemas de Informacion","Ingenieria de Calidad"];
+array.push(array52);
+
+var array53=["53","Seminario materia electiva","4to. cuat. Aprobado"];
+array.push(array53);
+
+var array54=["54","Factor Humano","Teoria de la Empresa"];
+array.push(array54);
+
+var array55=["55","Introduccion a la Gestion de Cadena de Suministro","Teoria de la Empresa"];
+array.push(array55);
+
+var array56=["56","Economia Politica","Teoria de la Empresa"];
+array.push(array56);
+
+var array57=["57","Mecanica Racional","Fisica 1","Analisis Matematico 3"];
+array.push(array57);
+
+var array58=["58","Resistencia de Materiales","Analisis Matematico 2","Fisica 1"];
+array.push(array58);
+
+var array59=["59","Actuadores","Mecanica de Fluidos", "Electrotecnia"];
+array.push(array59);
+
+var array60=["60","Mecatronica","Circuitos Electronicos"];
+array.push(array60);
+
+var array61=["61","Robotica","Actuadores","Sistemas de Adquisicion y Administracion de Datos"];
+array.push(array61);
+
+var array62=["62","Evaluacion de Proyectos","Contabilidad y Presupuestos"];
+array.push(array62);
